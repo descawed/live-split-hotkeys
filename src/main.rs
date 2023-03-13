8034c892f6cab80108e04b5f51d83c45e597bee9
descawed <github@descawed.com>
2023-03-12 20:34:43 -0400

    Mostly working now. Remaining work - read from all keyboards and break out into modules

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
+use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::mem::size_of;
 use std::ptr::addr_of;
+use std::thread;
+use std::time::Duration;
 
 use anyhow::{anyhow, Result};
 use clap::Parser;
 use input_event_codes_hashmap::{EV, KEY};
 use libc::{c_ulong, input_event, timeval};
 use procfs::process;
-use quick_xml::Reader;
+use quick_xml::Reader as XmlReader;
+use quick_xml::events::Event as XmlEvent;
 use x11rb::{atom_manager, connect};
 use x11rb::connection::{Connection, RequestConnection};
 use x11rb::protocol::Event;
 use x11rb::protocol::xkb::{self, ConnectionExt as _};
-use x11rb::protocol::xproto::{Atom, AtomEnum, ConnectionExt as _, EventMask, GrabMode, KEY_PRESS_EVENT, KEY_RELEASE_EVENT, KeyButMask, KeyPressEvent, ModMask, Window};
+use x11rb::protocol::xproto::{Atom, AtomEnum, ConnectionExt as _, EventMask, GrabMode, KEY_PRESS_EVENT, KEY_RELEASE_EVENT, Keycode as X11rbKeycode, KeyButMask, KeyPressEvent, ModMask, Window};
 use x11rb::xcb_ffi::XCBConnection;
 use xcb::x::GRAB_ANY;
 use xkbcommon::xkb as xkbc;
@@ -29,22 +33,30 @@ atom_manager! {
     }
 }
 
+const NUM_HOTKEYS: usize = 8;
+
 /// Listen for LiveSplit hotkeys
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Path to LiveSplit's settings.cfg where the hotkeys will be read from
     #[arg(short, long)]
-    settings_path: Option<String>,
+    settings: Option<String>,
     /// PID of the LiveSplit process to help identify the correct window
-    #[args(short, long)]
+    #[arg(short, long)]
     pid: Option<i32>,
     /// X window ID of the LiveSplit window
     #[arg(short, long)]
-    window_id: Option<u32>,
+    window: Option<u32>,
+    /// Path to the keyboard device file(s) to read from
+    #[arg(short, long)]
+    devices: Vec<String>,
+    /// How long to wait, in milliseconds, between pressing and releasing keys
+    #[arg(short, long, default_value_t=50)]
+    wait: u64,
 }
 
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 enum WindowMatch {
     FullMatch(Window),
     NameMatch(Window),
@@ -70,12 +82,206 @@ impl Into<Option<Window>> for WindowMatch {
     }
 }
 
+#[derive(Debug)]
+struct Keymapper {
+    key_map: HashMap<&'static str, &'static str>,
+}
+
+impl Keymapper {
+    pub fn new() -> Self {
+        let key_map = HashMap::from([
+            ("Control", "LEFTCTRL"),
+            ("ControlKey", "LEFTCTRL"),
+            ("LControlKey", "LEFTCTRL"),
+            ("Alt", "LEFTALT"),
+            ("LMenu", "LEFTALT"),
+            ("Back", "BACKSPACE"),
+            ("Capital", "CAPSLOCK"),
+            ("Escape", "ESC"),
+            ("LShiftKey", "LEFTSHIFT"),
+            ("Shift", "LEFTSHIFT"),
+            ("ShiftKey", "LEFTSHIFT"),
+            ("LWin", "LEFTMETA"),
+            ("Next", "PAGEDOWN"),
+            ("Prior", "PAGEUP"),
+            ("D0", "0"),
+            ("D1", "1"),
+            ("D2", "2"),
+            ("D3", "3"),
+            ("D4", "4"),
+            ("D5", "5"),
+            ("D6", "6"),
+            ("D7", "7"),
+            ("D8", "8"),
+            ("D9", "9"),
+            ("NumPad0", "KP0"),
+            ("NumPad1", "KP1"),
+            ("NumPad2", "KP2"),
+            ("NumPad3", "KP3"),
+            ("NumPad4", "KP4"),
+            ("NumPad5", "KP5"),
+            ("NumPad6", "KP6"),
+            ("NumPad7", "KP7"),
+            ("NumPad8", "KP8"),
+            ("NumPad9", "KP9"),
+            ("OemBackslash", "BACKSLASH"),
+            ("OemClear", "CLEAR"),
+            ("OemCloseBrackets", "RIGHTBRACE"),
+            ("Oemcomma", "COMMA"),
+            ("OemMinus", "MINUS"),
+            ("OemOpenBrackets", "LEFTBRACE"),
+            ("OemPeriod", "DOT"),
+            ("OemPipe", "BACKSLASH"),
+            ("Oemplus", "EQUAL"),
+            ("OemQuestion", "SLASH"),
+            ("OemQuotes", "APOSTROPHE"),
+            ("OemSemicolon", "SEMICOLON"),
+            ("Oemtilde", "GRAVE"),
+            ("RControlKey", "RIGHTCTRL"),
+            ("Return", "ENTER"),
+            ("RShiftKey", "RIGHTSHIFT"),
+            ("RWin", "RIGHTMETA"),
+            ("Scroll", "SCROLLLOCK"),
+        ]);
+
+        Keymapper { key_map }
+    }
+
+    pub fn map(&self, name: &str) -> Option<u32> {
+        if let Some(mapped_name) = self.key_map.get(name).copied() {
+            KEY.get(mapped_name).copied()
+        } else {
+            let uc_name = name.to_uppercase();
+            KEY.get(uc_name.as_str()).copied()
+        }
+    }
+
+    pub fn map_combo(&self, combo: &str) -> Result<Vec<u32>> {
+        let mut vec = Vec::new();
+        for key in combo.split(',') {
+            let key = key.trim();
+            if let Some(code) = self.map(key) {
+                vec.push(code);
+            } else {
+                return Err(anyhow!("Could not find mapping for {} in key combo {}", key, combo));
+            }
+        }
+
+        Ok(vec)
+    }
+}
+
+#[derive(Debug)]
+struct KeyState {
+    min_keycode: Keycode,
+    state: Vec<bool>,
+    hotkeys: Vec<Vec<u32>>,
+    toggle_hotkey: Option<Vec<u32>>,
+    hotkeys_enabled: bool,
+}
+
+impl KeyState {
+    pub fn new(xcb_conn: &xcb::Connection, settings_path: Option<&str>) -> Result<Self> {
+        // get keyboard state. documentation: https://xkbcommon.org/doc/current/group__x11.html
+        let context = xkbc::Context::new(xkbc::CONTEXT_NO_FLAGS);
+        let device_id = xkbc::x11::get_core_keyboard_device_id(xcb_conn);
+        let keymap = xkbc::x11::keymap_new_from_device(
+            &context,
+            xcb_conn,
+            device_id,
+            xkbc::KEYMAP_COMPILE_NO_FLAGS,
+        );
+
+        let min_keycode = keymap.min_keycode();
+        let mut hotkeys = Vec::with_capacity(NUM_HOTKEYS - 1);
+        let mut toggle_hotkey = None;
+        let mapper = Keymapper::new();
+
+        // read LiveSplit settings
+        let mut reader = match settings_path {
+            Some(s) => XmlReader::from_file(s),
+            None => XmlReader::from_file(env::var("HOME")? + "/LiveSplit/settings.cfg"),
+        }?;
+        reader.trim_text(true);
+        let mut expect = XmlExpect::None;
+        let mut hotkeys_enabled = true;
+        let mut buf = Vec::new();
+        loop {
+            match reader.read_event_into(&mut buf)? {
+                XmlEvent::Start(e) => {
+                    expect = match e.name().as_ref() {
+                        b"SplitKey" | b"ResetKey" | b"SkipKey" | b"UndoKey" | b"PauseKey" | b"SwitchComparisonPrevious" | b"SwitchComparisonNext" => XmlExpect::Hotkey,
+                        b"ToggleGlobalHotkeys" => XmlExpect::ToggleHotkey,
+                        b"GlobalHotkeysEnabled" => XmlExpect::HotkeysEnabled,
+                        _ => XmlExpect::None,
+                    };
+                }
+                XmlEvent::Text(e) => {
+                    match expect {
+                        XmlExpect::Hotkey => hotkeys.push(mapper.map_combo(e.unescape()?.as_ref())?),
+                        XmlExpect::ToggleHotkey => toggle_hotkey = Some(mapper.map_combo(e.unescape()?.as_ref())?),
+                        XmlExpect::HotkeysEnabled => hotkeys_enabled = e.unescape()?.trim().eq_ignore_ascii_case("true"),
+                        _ => (),
+                    }
+                }
+                XmlEvent::End(_) => expect = XmlExpect::None,
+                XmlEvent::Eof => break,
+                _ => (),
+            }
+        }
+
+        let num_keys = KEY.iter().map(|(_, code)| *code).max().unwrap() as usize;
+
+        Ok(Self {
+            min_keycode,
+            state: vec![false; num_keys],
+            hotkeys,
+            toggle_hotkey,
+            hotkeys_enabled,
+        })
+    }
+
+    fn check_hotkey(&self, key: u32, hotkey: &[u32]) -> bool {
+        hotkey.iter().any(|c| *c == key) && hotkey.iter().all(|c| self.state[*c as usize])
+    }
+
+    pub fn handle_key(&mut self, key: u32, is_pressed: bool) -> Vec<Vec<Keycode>> {
+        let mut result = Vec::with_capacity(NUM_HOTKEYS);
+        self.state[key as usize] = is_pressed;
+        if let Some(toggle_hotkey) = self.toggle_hotkey.as_deref() {
+            if self.check_hotkey(key, toggle_hotkey) {
+                self.hotkeys_enabled = !self.hotkeys_enabled;
+                result.push(toggle_hotkey.iter().map(|c| (*c as Keycode) + self.min_keycode).collect());
+            }
+        }
+
+        if self.hotkeys_enabled {
+            for hotkey in &self.hotkeys {
+                if self.check_hotkey(key, hotkey) {
+                    result.push(hotkey.iter().map(|c| (*c as Keycode) + self.min_keycode).collect());
+                }
+            }
+        }
+
+        result
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum XmlExpect {
+    Hotkey,
+    ToggleHotkey,
+    HotkeysEnabled,
+    None,
+}
+
 struct HotkeyListener {
     args: Args,
     xcb_conn: xcb::Connection,
     screen_num: usize,
     conn: XCBConnection,
     atoms: AtomCollection,
+    key_state: KeyState,
 }
 
 impl HotkeyListener {
@@ -98,48 +304,32 @@ impl HotkeyListener {
         let atoms = atoms.reply()?;
         let xkb = xkb.reply()?;
         if !xkb.supported {
-            Err(anyhow!("xkb extension not supported"))
-        } else {
-            Ok(HotkeyListener {
-                args,
-                xcb_conn,
-                screen_num,
-                conn,
-                atoms,
-            })
+            return Err(anyhow!("xkb extension not supported"));
         }
+
+        let key_state = KeyState::new(&xcb_conn, args.settings.as_deref())?;
+        Ok(Self {
+            args,
+            xcb_conn,
+            screen_num,
+            conn,
+            atoms,
+            key_state,
+        })
     }
 
-    pub fn listen(self) -> Result<()> {
-        // get keyboard state. documentation: https://xkbcommon.org/doc/current/group__x11.html
-        let context = xkbc::Context::new(xkbc::CONTEXT_NO_FLAGS);
-        let device_id = xkbc::x11::get_core_keyboard_device_id(&self.xcb_conn);
-        let keymap = xkbc::x11::keymap_new_from_device(
-            &context,
-            &self.xcb_conn,
-            device_id,
-            xkbc::KEYMAP_COMPILE_NO_FLAGS,
-        );
-        let mut state = xkbc::x11::state_new_from_device(&keymap, &self.xcb_conn, device_id);
-
-        // we're going to watch for the individual keys listed in the LiveSplit config, so let's make a
-        // map to look up which key code corresponds to each key name
-        let sym_to_code: HashMap<Keysym, Keycode> = (keymap.min_keycode()..=keymap.max_keycode())
-            .flat_map(|c| state.key_get_syms(c).into_iter().filter_map(move |s| {
-                match *s {
-                    xkbc::keysyms::KEY_NoSymbol | xkbc::keysyms::KEY_VoidSymbol => None,
-                    _ => Some((*s, c)),
-                }
-            })).collect();
-
+    pub fn listen(mut self) -> Result<()> {
         let screen = &self.conn.setup().roots[self.screen_num];
-        let window = match self.args.window_id {
+        let window = match self.args.window {
             Some(id) => id,
             None => self.find_window(screen.root)?.ok_or_else(|| anyhow!("LiveSplit window not found"))?,
         };
         println!("Window found: {}", window);
 
         let ev_key = EV["KEY"] as u16;
+        let event_sequence = [(KEY_RELEASE_EVENT, EventMask::KEY_RELEASE, true), (KEY_PRESS_EVENT, EventMask::KEY_PRESS, true), (KEY_RELEASE_EVENT, EventMask::KEY_RELEASE, false)];
+        let key_delay = Duration::from_millis(self.args.wait);
+
         let mut event_buf = [0u8; size_of::<input_event>()];
         // TODO: look up keyboard device path
         let mut file = File::open("/dev/input/by-path/pci-0000:00:14.0-usb-0:5.2.4:1.0-event-kbd")?;
@@ -148,24 +338,48 @@ impl HotkeyListener {
             // I don't think this is that bad because an input_event is ultimately all ints, so there are no invalid
             // bit patterns, and binrw would just be reading the exact same bytes in the exact same sequence.
             let event = unsafe { &*(addr_of!(event_buf) as *const input_event) };
-            if event.type_ == ev_key {
-                let event_to_send = KeyPressEvent {
-                    response_type: if event.value == 0 { KEY_RELEASE_EVENT } else { KEY_PRESS_EVENT },
-                    detail: 42,
-                    sequence: 0,
-                    time: x11rb::CURRENT_TIME,
-                    root: screen.root,
-                    event: window,
-                    child: x11rb::NONE,
-                    root_x: 1,
-                    root_y: 1,
-                    event_x: 1,
-                    event_y: 1,
-                    state: KeyButMask::CONTROL,
-                    same_screen: true,
-                };
-                self.conn.send_event(true, window, if event.value == 0 { EventMask::KEY_RELEASE } else { EventMask::KEY_PRESS }, event_to_send)?.check()?;
-                println!("{:#x} was {}", event.code, if event.value == 0 { "released" } else { "pressed" });
+            // 2 = autorepeat, which we don't want to listen for
+            if event.type_ == ev_key && event.value < 2 {
+                println!("Key {} = {}", event.code, event.value);
+                let raw_code = event.code as u32;
+                let active_hotkeys = self.key_state.handle_key(raw_code, event.value != 0);
+                if !active_hotkeys.is_empty() {
+                    let focused_window = self.conn.get_input_focus()?.reply()?.focus;
+                    if focused_window == window {
+                        println!("Not sending hotkey because LiveSplit already has focus");
+                        continue;
+                    }
+                }
+
+                for keys_to_send in active_hotkeys {
+                    println!("Sending hotkey {:?}", keys_to_send);
+                    let mut event_to_send = KeyPressEvent {
+                        response_type: KEY_PRESS_EVENT,
+                        detail: 0,
+                        sequence: 0,
+                        time: x11rb::CURRENT_TIME,
+                        root: screen.root,
+                        event: window,
+                        child: x11rb::NONE,
+                        root_x: 1,
+                        root_y: 1,
+                        event_x: 1,
+                        event_y: 1,
+                        state: KeyButMask::CONTROL,
+                        same_screen: true,
+                    };
+
+                    for (response_type, mask, do_sleep) in event_sequence.iter().copied() {
+                        for key in &keys_to_send {
+                            event_to_send.response_type = response_type;
+                            event_to_send.detail = *key as X11rbKeycode;
+                            self.conn.send_event(true, window, mask, event_to_send)?.check()?;
+                        }
+                        if do_sleep {
+                            thread::sleep(key_delay);
+                        }
+                    }
+                }
             }
         }
     }
